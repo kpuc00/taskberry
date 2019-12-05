1de006515907e68b15a12cc1d29b871411397703
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2019-12-05 14:16:30 +0100

    Added connection class
    Needs modifications

---
+++ Tenant_Application/Tenant_Application/DeviceConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace connection
+{
+    public class DeviceConnection
+    {
+        private string stringToSend;
+        private void serverThread()
+        {
+            UdpClient udpClient = new UdpClient(8888);
+            while (true)
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returndata = Encoding.ASCII.GetString(receiveBytes);
+
+                //orderNum = returndata.Split('#').First();
+                //returndata = returndata.Replace(orderNum + "#", "");
+
+                //pizzaNum = returndata.Split('%').First();
+                //returndata = returndata.Replace(pizzaNum + "%", "");
+                //message = returndata.Split('&').First();
+
+                //lbxOrders.Items.Add($"{orderNum} \t {pizzaNum} \t {message}");
+                //data.Add($"{orderNum}#{pizzaNum}%{message}&*");
+            }
+        }
+        private void sendData()
+        {
+            string ip = "";
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(ip, 8888);
+            Byte[] sendData = Encoding.ASCII.GetBytes(stringToSend);
+            udpClient.Send(sendData, sendData.Length);
+        }
+    }
+}
