a552dbfd86ecd50b38fb5fb8c8514ec945c6b60c
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2019-12-05 13:16:44 +0100

    initial Start

---
+++ Tenant_Application/Tenant_Application/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Tenant_Application
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.tbxUserName = new System.Windows.Forms.TextBox();
+            this.tbxPassWord = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbxUserName
+            // 
+            this.tbxUserName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbxUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxUserName.Location = new System.Drawing.Point(363, 73);
+            this.tbxUserName.Name = "tbxUserName";
+            this.tbxUserName.Size = new System.Drawing.Size(100, 37);
+            this.tbxUserName.TabIndex = 0;
+            this.tbxUserName.Click += new System.EventHandler(this.TbxUserName_Click);
+            // 
+            // tbxPassWord
+            // 
+            this.tbxPassWord.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxPassWord.Location = new System.Drawing.Point(75, 14);
+            this.tbxPassWord.Name = "tbxPassWord";
+            this.tbxPassWord.Size = new System.Drawing.Size(100, 24);
+            this.tbxPassWord.TabIndex = 1;
+            this.tbxPassWord.Click += new System.EventHandler(this.TbxPassWord_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(430, 182);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(146, 204);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Controls.Add(this.tbxUserName);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.tbxPassWord);
+            this.panel3.Location = new System.Drawing.Point(149, 54);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(940, 374);
+            this.panel3.TabIndex = 0;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1174, 649);
+            this.Controls.Add(this.panel3);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxUserName;
+        private System.Windows.Forms.TextBox tbxPassWord;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Tenant_Application/Tenant_Application/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tenant_Application
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        int CenterLocationFormX;
+        int CenterLocationFormY;
+
+        int CenterLocationTextBoxX;
+        int CenterLocationTextBoxY;
+
+        int CenterLocationPanel3X;
+        int CenterLocationPanel3Y;
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            //Init the values of the LoginForm
+            this.Width = 1200;
+            this.Height = 720;
+
+            CenterLocationFormX = this.Width / 2;
+            CenterLocationFormY = this.Height / 2;
+
+            this.BackgroundImage = 
+
+            //General Values of TextBox
+            CenterLocationTextBoxX = tbxUserName.Width / 2;
+            CenterLocationTextBoxY = tbxUserName.Height / 2;
+
+            //UserName TextBox
+            tbxUserName.Width = 200;
+
+            tbxUserName.Font = new Font("Calibri", 12);
+
+            tbxUserName.Text = "UserName";
+
+            tbxUserName.BorderStyle = BorderStyle.None;
+
+            tbxUserName.Location = new Point(CenterLocationFormX - CenterLocationTextBoxX, (CenterLocationFormY - CenterLocationTextBoxY) - 25);
+
+            tbxUserName.BackColor = this.BackColor;
+
+            //PassWord TextBox
+            tbxPassWord.Width = 200;
+
+            tbxPassWord.Font = new Font("Calibri", 12);
+
+            tbxPassWord.Text = "PassWord";
+
+            tbxPassWord.BorderStyle = BorderStyle.None;
+
+            tbxPassWord.Location = new Point(CenterLocationFormX - CenterLocationTextBoxX, (CenterLocationFormY - CenterLocationTextBoxY) + 25);
+
+            tbxPassWord.BackColor = this.BackColor;
+
+            //Panel1
+            panel1.Width = 200;
+            panel1.Height = 1;
+
+            panel1.BackColor = Color.White;
+
+            panel1.Location = new Point(CenterLocationFormX - CenterLocationTextBoxX, ((CenterLocationFormY - CenterLocationTextBoxY) - 25) + tbxUserName.Height + 5);
+
+            //Panel2
+            panel2.Width = 200;
+            panel2.Height = 1;
+
+            panel2.BackColor = Color.White;
+
+            panel2.Location = new Point(CenterLocationFormX - CenterLocationTextBoxX, ((CenterLocationFormY - CenterLocationTextBoxY) + 25) + tbxUserName.Height + 5);
+
+            //Panel3
+            panel3.Width = 400;
+            panel3.Height = 400;
+
+            CenterLocationPanel3X = panel3.Width / 2;
+            CenterLocationPanel3Y = panel3.Height / 2;
+
+            panel3.BackColor = Color.FromArgb(83, 83 , 83);
+
+            panel3.Location = new Point(CenterLocationFormX - CenterLocationPanel3X, CenterLocationFormY - CenterLocationPanel3Y);
+
+        }
+
+        private void TbxUserName_Click(object sender, EventArgs e)
+        {
+            tbxUserName.Clear();
+        }
+
+        private void TbxPassWord_Click(object sender, EventArgs e)
+        {
+            tbxPassWord.Clear();
+            tbxPassWord.PasswordChar = '*';
+        }
+
+
+    }
+}
