d2f513f7839d377bfc28a47bf8980441b8add6d5
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2019-12-05 13:58:41 +0100

    Improved original

--- Tenant_Application/Tenant_Application/Form1.Designer.cs
+++ Tenant_Application/Tenant_Application/Form1.Designer.cs
@@ -29,55 +29,52 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.tbxUserName = new System.Windows.Forms.TextBox();
             this.tbxPassWord = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbxUserName = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tbxPassWord
+            // 
+            this.tbxPassWord.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxPassWord.Location = new System.Drawing.Point(12, 228);
+            this.tbxPassWord.Name = "tbxPassWord";
+            this.tbxPassWord.Size = new System.Drawing.Size(100, 24);
+            this.tbxPassWord.TabIndex = 1;
+            this.tbxPassWord.Click += new System.EventHandler(this.TbxPassWord_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(251, 526);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(757, 504);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 2;
             // 
             // tbxUserName
             // 
             this.tbxUserName.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.tbxUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxUserName.Location = new System.Drawing.Point(363, 73);
+            this.tbxUserName.Location = new System.Drawing.Point(44, 515);
             this.tbxUserName.Name = "tbxUserName";
             this.tbxUserName.Size = new System.Drawing.Size(100, 37);
             this.tbxUserName.TabIndex = 0;
             this.tbxUserName.Click += new System.EventHandler(this.TbxUserName_Click);
             // 
-            // tbxPassWord
-            // 
-            this.tbxPassWord.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tbxPassWord.Location = new System.Drawing.Point(75, 14);
-            this.tbxPassWord.Name = "tbxPassWord";
-            this.tbxPassWord.Size = new System.Drawing.Size(100, 24);
-            this.tbxPassWord.TabIndex = 1;
-            this.tbxPassWord.Click += new System.EventHandler(this.TbxPassWord_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(430, 182);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(146, 204);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 0;
-            // 
             // panel3
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel3.Controls.Add(this.tbxUserName);
-            this.panel3.Controls.Add(this.panel1);
-            this.panel3.Controls.Add(this.panel2);
-            this.panel3.Controls.Add(this.tbxPassWord);
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel3.Location = new System.Drawing.Point(149, 54);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(940, 374);
@@ -88,23 +85,27 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1174, 649);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tbxUserName);
             this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.tbxPassWord);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbxUserName;
         private System.Windows.Forms.TextBox tbxPassWord;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbxUserName;
         private System.Windows.Forms.Panel panel3;
     }
 }
--- Tenant_Application/Tenant_Application/Form1.cs
+++ Tenant_Application/Tenant_Application/Form1.cs
@@ -34,8 +34,6 @@ namespace Tenant_Application
             CenterLocationFormX = this.Width / 2;
             CenterLocationFormY = this.Height / 2;
 
-            this.BackgroundImage = 
-
             //General Values of TextBox
             CenterLocationTextBoxX = tbxUserName.Width / 2;
             CenterLocationTextBoxY = tbxUserName.Height / 2;
@@ -45,18 +43,24 @@ namespace Tenant_Application
 
             tbxUserName.Font = new Font("Calibri", 12);
 
+            tbxUserName.BringToFront();
+
             tbxUserName.Text = "UserName";
 
             tbxUserName.BorderStyle = BorderStyle.None;
 
             tbxUserName.Location = new Point(CenterLocationFormX - CenterLocationTextBoxX, (CenterLocationFormY - CenterLocationTextBoxY) - 25);
 
-            tbxUserName.BackColor = this.BackColor;
+            tbxUserName.BackColor = Color.FromArgb(83, 83, 83);
 
             //PassWord TextBox
             tbxPassWord.Width = 200;
 
             tbxPassWord.Font = new Font("Calibri", 12);
+
+            tbxPassWord.BringToFront();
+
+
 
             tbxPassWord.Text = "PassWord";
 
@@ -70,6 +74,8 @@ namespace Tenant_Application
             panel1.Width = 200;
             panel1.Height = 1;
 
+            panel1.BringToFront();
+
             panel1.BackColor = Color.White;
 
             panel1.Location = new Point(CenterLocationFormX - CenterLocationTextBoxX, ((CenterLocationFormY - CenterLocationTextBoxY) - 25) + tbxUserName.Height + 5);
@@ -77,6 +83,8 @@ namespace Tenant_Application
             //Panel2
             panel2.Width = 200;
             panel2.Height = 1;
+
+            panel2.BringToFront();
 
             panel2.BackColor = Color.White;
 
@@ -86,10 +94,12 @@ namespace Tenant_Application
             panel3.Width = 400;
             panel3.Height = 400;
 
+
+
             CenterLocationPanel3X = panel3.Width / 2;
             CenterLocationPanel3Y = panel3.Height / 2;
 
-            panel3.BackColor = Color.FromArgb(83, 83 , 83);
+            panel3.BackColor = Color.Transparent;
 
             panel3.Location = new Point(CenterLocationFormX - CenterLocationPanel3X, CenterLocationFormY - CenterLocationPanel3Y);
 
