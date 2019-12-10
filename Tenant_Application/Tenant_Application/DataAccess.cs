0c1666561f244c061a62915525fa3ce5163c6b9d
Michael <bahchevanov.mihail@gmail.com>
2019-12-10 14:58:58 +0100

    Updates classes and added a Chore class
    Changes need to be done

--- Tenant_Application/Tenant_Application/DataAccess.cs
+++ Tenant_Application/Tenant_Application/DataAccess.cs
@@ -10,13 +10,45 @@ namespace Tenant_Application
 {
     public class DataAccess
     {
-        public List<Account> GetCredentials(string account)
+        public List<Account> GetUsername(string username)
         {
             //creating a new connection to our sql database
-            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TaskBerryDB")))
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("dbi428024")))
             {
                 //get the open connection and ask for the type account data and command issue
-                var output = connection.Query<Account>($"select Username, Password, EmailAddress from Account where Username = '{ account }'").ToList();
+                var output = connection.Query<Account>("dbo.Account_GetByUsername @Text", new { Text = username }).ToList();
+                return output;
+            }
+        }
+        public List<Account> GetPassword(string password)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("dbi428024")))
+            {
+                var output = connection.Query<Account>("dbo.Account_GetByPassword @Text", new { Text = password }).ToList();
+                return output;
+            }
+        }
+        public List<Account> GetName(string name)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("dbi428024")))
+            {
+                var output = connection.Query<Account>("dbo.Account_GetByName @Text", new { Text = name }).ToList();
+                return output;
+            }
+        }
+        public List<Account> GetId(int id)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("dbi428024")))
+            {
+                var output = connection.Query<Account>("dbo.Account_GetById @Id", new { Id = id }).ToList();
+                return output;
+            }
+        }
+        public List<Chore> GetChore(string chore)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("dbi428024")))
+            {
+                var output = connection.Query<Chore>("dbo.Account_GetByUsername @Text", new { Text = chore }).ToList();
                 return output;
             }
         }
