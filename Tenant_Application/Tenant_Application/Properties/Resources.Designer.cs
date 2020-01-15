f7037646b1df6eac9e44832287bfdde9b08e69b1
Aleksandar Todorov <alex20009@abv.bg>
2020-01-15 20:49:49 +0100

    GUI improvements

--- Tenant_Application/Tenant_Application/Properties/Resources.Designer.cs
+++ Tenant_Application/Tenant_Application/Properties/Resources.Designer.cs
@@ -113,6 +113,26 @@ namespace Tenant_Application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap legend1 {
+            get {
+                object obj = ResourceManager.GetObject("legend1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap legend2 {
+            get {
+                object obj = ResourceManager.GetObject("legend2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap loginPaneObject {
             get {
                 object obj = ResourceManager.GetObject("loginPaneObject", resourceCulture);
